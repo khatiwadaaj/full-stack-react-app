4ea9255afb4403a0bb5c41ca74b5bb4da9033b64
Ajaya Khatiwada <khatiwadaajaya@gmail.com>
2018-10-31 10:58:12 +1100

    finishing up the react-router

---
+++ src/containers/Template.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Template extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>TicTacTuring</h1>
+        </header>
+        <main>{this.props.children}</main>
+      </div>
+    );
+  }
+}
+
+export default Template;
